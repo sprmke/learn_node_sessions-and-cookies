a662dd7a8e14dd35a8de7a5f0e8c638f7836871e
Michael Manlulu <michael.manlulu@myresman.com>
2022-10-11 21:48:23 +0800

    239. initializeing the session middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const dotenv = require('dotenv');
 
@@ -22,6 +23,9 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
