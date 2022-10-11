4dcce37227df836354b6517e376e247fcace6d7b
Michael Manlulu <michael.manlulu@myresman.com>
2022-10-11 10:56:50 +0800

    235. setting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,15 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').trim().split('=')[1];
+  console.log('isLoggedIn', isLoggedIn);
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-  };
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
+};
